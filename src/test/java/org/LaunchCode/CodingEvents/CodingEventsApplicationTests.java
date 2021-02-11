a6653585eb43cabb085e87f1c70ce25729410405
JT-B <jtbellavia03@gmail.com>
2021-02-11 16:36:00 -0600

    Initial commit

---
+++ src/test/java/org/LaunchCode/CodingEvents/CodingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.CodingEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
