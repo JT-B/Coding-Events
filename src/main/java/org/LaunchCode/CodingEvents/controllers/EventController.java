910ee4cca065ad332874eb82ddc9d844a98bab57
JT-B <jtbellavia03@gmail.com>
2021-02-16 16:39:00 -0600

    delete functionality added

--- src/main/java/org/LaunchCode/CodingEvents/controllers/EventController.java
+++ src/main/java/org/LaunchCode/CodingEvents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.LaunchCode.CodingEvents.controllers;
 
+import org.LaunchCode.CodingEvents.Data.EventData;
 import org.LaunchCode.CodingEvents.Models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +16,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -32,7 +32,25 @@ public class EventController {
     //lives at /events/create
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:";
     }
 
